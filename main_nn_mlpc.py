9419358879d21f0fac33f87cbe7c78e601ca6d47
Liang Entao <liangsitao.gmail.com>
2018-11-02 21:25:57 +0800

    1. Print the evaluation summary of the final model.

--- main_nn_mlpc.py
+++ main_nn_mlpc.py
@@ -13,7 +13,7 @@ from keras.models import Input
 
 from common.dataprep import definitions, get_dataset
 from models.nn_mlpc import create_mlpc
-from common.utils import compile_and_train_early_stop, visualize_history
+from common.utils import compile_and_train_early_stop, visualize_history, evaluate_accuracy
 
 np.random.seed(1337)
 
@@ -79,3 +79,21 @@ print("ske average loss: " + str(avg_loss_ske / 5))
 print("ske average accuracy: " + str(avg_val_acc_ske / 5))
 print("iner average loss: " + str(avg_loss_iner / 5))
 print("iner average accuracy: " + str(avg_val_acc_iner / 5))
+
+print("\n\nEvaluation Summary")
+for i in range(5):
+    X_train_iner = trainX_iner[i]
+    y_train_iner = trainY_iner[i]
+    X_test_iner = testX_iner[i]
+    y_test_iner = testY_iner[i]
+
+    X_train_ske = trainX_ske[i]
+    y_train_ske = trainY_ske[i]
+    X_test_ske = testX_ske[i]
+    y_test_ske = testY_ske[i]
+
+    print("\nSubject index: " + str(i))
+    acc_iner = evaluate_accuracy(model_iner, X_test_iner, y_test_iner)
+    acc_ske = evaluate_accuracy(model_ske, X_test_ske, y_test_ske)
+    print("::iner accuracy: " + str(acc_iner))
+    print("::ske accuracy: " + str(acc_ske))
